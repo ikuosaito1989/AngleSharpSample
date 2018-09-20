4d16493fa68156a00f0f3d92266761d81c0bc890
斉藤育男 <ikuo@saitouikuonoMacBook-ea.local>
2018-09-21 00:17:24 +0900

    first Commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using AngleSharp.Parser.Html;
+
+namespace AngleSharpSample {
+    class Program {
+        static void Main (string[] args) {
+            AngleSharpSample ().Wait ();
+        }
+
+        public static async Task AngleSharpSample () {
+            var url = "https://mygkrnk.com/";
+            var client = new HttpClient ();
+            var response = await client.GetAsync (url);
+            var html = await response.Content.ReadAsStringAsync ();
+
+            var parser = new HtmlParser ();
+            var doc = parser.Parse (html);
+            var likeApp = doc.GetElementsByClassName ("like-app");
+            var count = likeApp.First().Attributes["react-prop-counts"].Value;
+
+            var titles = doc.GetElementsByClassName ("entry-title");
+            var title = titles.Where(x => x.InnerHtml.Contains("In My Feelings")).First().InnerHtml;
+
+            Console.Write(count);
+            Console.Write(title);
+        }
+    }
+}
